3efc777dc9bce859f72e0d700500da1195943bde
tojawawel <bgawel93@gmail.com>
2018-12-01 11:24:26 +0100

    add create simple form for article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,12 @@ class ArticlesController < ApplicationController
   def index
   end
 
+  def new
+    # @article = Article.new
+  end
+
+  def create
+    render plain: params[:article].inspect
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'welcome/index'
   get 'articles', to: 'articles#index'
+  get 'articles/new', to:'articles#new'
+  post 'articles', to: 'articles#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
