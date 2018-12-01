08706cda640c280a268f4d7a71257d060bb090ab
tojawawel <bgawel93@gmail.com>
2018-12-01 13:43:10 +0100

    fix routing name mistake

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'welcome/index'
   get 'articles', to: 'articles#index'
-  get 'articles/new', to:'articles#new'
+  get 'articles/new', to:'articles#new', as: 'new_article'
   post 'articles', to: 'articles#create'
   get 'articles/:id', to: 'articles#show', as: 'article'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
