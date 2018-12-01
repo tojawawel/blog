f64a6028ef089148848df1b4db68bd53b775f662
tojawawel <bgawel93@gmail.com>
2018-12-01 12:32:52 +0100

    redirecting after saving article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -10,7 +10,7 @@ class ArticlesController < ApplicationController
     article_params = params.require(:article).permit(:title, :text)
     @article = Article.new(article_params)
     @article.save
-    redirect_to articles_path
+    redirect_to article_path(@article)
   end
 
   def show
