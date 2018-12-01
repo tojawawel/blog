c064aab3d180ea64192eeeb489ee787a67e0a0d3
tojawawel <bgawel93@gmail.com>
2018-12-01 14:58:31 +0100

    add edit for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -21,4 +21,18 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    article_params = params.require(:article).permit(:title, :text)
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+
 end
