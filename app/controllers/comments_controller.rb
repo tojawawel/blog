636cb088c132b40bc937d70cd7f647b579a2a3f0
tojawawel <bgawel93@gmail.com>
2018-12-02 12:17:03 +0100

    change comments routes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,9 @@
 class CommentsController < ApplicationController
-
+before_action :find_article
   def create
+    binding.pry
     @comment = Comment.new(comment_params)
+    @comment.article = @article
     @comment.save
     redirect_to article_path(@comment.article)
   end
@@ -9,7 +11,11 @@ class CommentsController < ApplicationController
 
   private
   def comment_params
-    params.require(:comment).permit(:commenter, :body, :article_id)
+    params.require(:comment).permit(:commenter, :body)
+  end
+
+  def find_article
+    @article = Article.find(params[:article_id])
   end
 
 end
