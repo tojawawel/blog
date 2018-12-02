0a4c09925b10df7230e070e7672cd5b94088ff1d
tojawawel <bgawel93@gmail.com>
2018-12-02 15:26:23 +0100

    add editing comments, amount of comments, date of comment creation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,7 @@
 class CommentsController < ApplicationController
-before_action :find_article
+  before_action :find_article
+  before_action :find_comment, only: [:destroy, :edit, :update]
+
   def create
     @comment = Comment.new(comment_params)
     @comment.article = @article
@@ -12,24 +14,31 @@ before_action :find_article
   end
 
   def destroy
-    @comment = Comment.find(params[:id])
     @comment.destroy
     redirect_to article_path(@comment.article)
   end
 
+  def edit
+  end
+
+  def update
+    if @comment.update(comment_params)
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
+
   private
   def comment_params
-    params.require(:comment).permit(:commenter, :body)
+    params.require(:comment).permit(:commenter, :body, :article_id)
   end
 
   def find_article
     @article = Article.find(params[:article_id])
   end
 
+  def find_comment
+    @comment = Comment.find(params[:id])
+  end
 end
-
-
-# @article = Article.find(params[:article_id])
-# @comment = @article.comments.new(comment_params)
-
-# end
