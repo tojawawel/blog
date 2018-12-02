173a39afb2a178c5836dcd7b73056c864f926059
tojawawel <bgawel93@gmail.com>
2018-12-02 12:57:43 +0100

    add simple format to comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,3 +2,6 @@ class Article < ApplicationRecord
   validates :title, presence: true, length: { minimum:5}
   has_many :comments, dependent: :destroy
 end
+
+
+#dependent pozwala na wypieprzenie calego artikla z podleglymi mu komentami
