25e9aef4884a389e5f2f5aa8148d3c1984ad5a35
tojawawel <bgawel93@gmail.com>
2018-12-15 09:42:07 +0100

    add email validations

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,9 @@
 class Comment < ApplicationRecord
-  validates :commenter, presence: true
-  validates :body, presence: true, length: {in: 6..500} 
+  include ActiveModel::Validations
+  attr_accessor :email
+
+  validates :commenter, presence: true, email: true
+  validates :body, presence: true, length: {in: 6..500}
   belongs_to :article
+
 end
