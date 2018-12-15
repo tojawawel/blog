1303eca3d6610fd0d07964cabb579314092ccfb1
tojawawel <bgawel93@gmail.com>
2018-12-15 10:18:46 +0100

    add constraints to comment and article in database

--- db/schema.rb
+++ db/schema.rb
@@ -10,21 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_02_085647) do
+ActiveRecord::Schema.define(version: 2018_12_15_090540) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
-    t.string "title"
+    t.string "title", null: false
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "comments", force: :cascade do |t|
-    t.string "commenter"
-    t.text "body"
+    t.string "commenter", null: false
+    t.text "body", null: false
     t.bigint "article_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
