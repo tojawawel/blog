ed211199b80398246b245f8b37e3cd8eeee9fcbc
tojawawel <bgawel93@gmail.com>
2019-02-02 13:19:02 +0100

    fix typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,12 +93,12 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
-  :user_name => ENV["SMTP_NAME"]
-  :password => ENV["SMTP_PASSWORD"]
-  :address => ENV["SMTP_ADRESS"]
-  :domain => ENV["SMTP_DOMAIN"]
-  :port => ENV["SMTP_PORT"]
-  :authentication => :cram_md5
-}
+  config.action_mailer.smtp_settings = {
+    :user_name => ENV["SMTP_NAME"],
+    :password => ENV["SMTP_PASSWORD"],
+    :address => ENV["SMTP_ADDRESS"],
+    :domain => ENV["SMTP_DOMAIN"],
+    :port => ENV["SMTP_PORT"],
+    :authentication => :cram_md5
+  }
 end
